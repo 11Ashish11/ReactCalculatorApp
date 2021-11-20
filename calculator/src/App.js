e4dd12cd8e413d2df137d83259c4e07476b0c4ea
11Ashish11 <ashish.bhoya.56@gmail.com>
2021-11-21 00:01:54 +0530

    Created Create-react-app

---
+++ calculator/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        This is Ashish Bhoya
+      </div>
+    );
+  }
+}
+
+export default App;
